428266501ae5721c7469072b355b32d12221c4d8
Shobhit9887 <44239978+Shobhit9887@users.noreply.github.com>
2020-05-18 19:09:16 +0530

    First commit

---
+++ craigrid/views.py
@@ -0,0 +1,59 @@
+import requests
+from django.shortcuts import render
+from requests.compat import quote_plus
+from bs4 import BeautifulSoup
+from . import models
+
+# Create your views here.
+BASE_CRAIGSLIST_URL = 'https://lucknow.craigslist.org/search/bbb?query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
+
+def home(request):
+    return render(request, 'base.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+    #print(quote_plus(search))
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    #print(data)
+    soup = BeautifulSoup(data, features='html.parser')
+    if_empty = soup.find('div', {'id': 'sortable-results'})
+    if(if_empty.find(class_='alert-warning')):
+        error = if_empty.find(class_='alert-warning').text
+    else:
+        error = ""
+
+    if(error == 'Nothing found for that search. (All words must match.)'):
+        search = "No results found for your search. Try something else"
+    else:
+        search = "You searched for: " + search
+
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+
+    final_postings = []
+
+    for post in post_listings:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+        else:
+            post_image_url = 'https://craigslist.org/images/peace.jpg'
+
+        final_postings.append((post_title, post_url, post_price, post_image_url))
+
+    stuff_for_frontend = {
+        'search': search,
+        'final_postings': final_postings,
+    }
+    return render(request, 'craigrid/new_search.html', stuff_for_frontend)
